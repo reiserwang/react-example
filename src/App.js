8617fc860c6e7284013ec3dbfbf78d189a4d9638
Reiser Wang <rewang@microsoft.com>
2019-06-06 14:43:11 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
           <h2>It's {new Data().toLocaleTimeString()}.</h2>
-          </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

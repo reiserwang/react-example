4e5bb8aeec8a9a265da4752f931bd4c6106b8762
Reiser Wang <rewang@microsoft.com>
2019-06-06 14:39:00 +0800

    setInterval

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <h2>It's {new Data().toLocaleTimeString()}.</h2>
+          </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -17,5 +19,6 @@ class App extends Component {
     );
   }
 }
+setInterval(render,1000);
 
 export default App;
